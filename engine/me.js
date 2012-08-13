89a2f89470a9d93726c17528adc8d5bd4f17ef36
Rodrigo Vidal <rodrigovidal777@gmail.com>
2012-08-13 19:23:08 -0300

    First commit

--- engine/me.js
+++ engine/me.js
@@ -1,16 +1,18 @@
 exports.cfg = {
-	"name" : "Diullei Gomes",
-	"github" : "https://www.github.com/Diullei",
-	"disqus" : "diulleigomes",
-	"twitter" : "http://www.twitter.com/diullei",
-	"twitterName" : "@diullei",
-	"linkedin" : "http://br.linkedin.com/in/diullei",
-	"email" : "diullei@gmail.com",
-	"gravatar" : "https://secure.gravatar.com/avatar/5e93890c5b54d18dd454f14789c2b225",
+	"name" : "Rodrigo Vidal",
+	"github" : "https://www.github.com/rodrigovidal",
+	"disqus" : "vidalgithub",
+	"twitter" : "http://www.twitter.com/rodrigovidal",
+	"twitterName" : "@rodrigovidal",
+	"linkedin" : "http://br.linkedin.com/in/rodrigovidal777",
+	"email" : "rodrigovidal777@gmail.com",
+	"gravatar" : "https://secure.gravatar.com/avatar/bfd8bb90fdf6e83c2f81b019fc74cc4f",
 	"blogroll" : [
-		{url: "http://rodrigovidal.net/", name: "Rodrigo Vidal"},
+		{url: "http://diullei.github.com", name: "Diullei"},
 		{url: "http://dotnetarchitects.net/", name: ".NET Architects"},
         {url: "http://elemarjr.net/", name: "Elemar DEV"},
-        {url: "http://backbonetutorials.com/", name: "Backbone Tutorials"}
+        
 	]
 };
+
+
